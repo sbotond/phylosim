2bd8506a482f2a759039baffcecb8797c7b7d831
Botond Sipos <sbotond@gmail.com>
2010-06-14 18:11:08 +0100

    * Added documentation to CodonSequence.R. * Moved the contents of CodonSequence.R into PopularSequences.R

--- PopularSequences.R
+++ PopularSequences.R
@@ -270,4 +270,250 @@ setConstructorS3(
   enforceRCC=TRUE
 );
 
+##
+## CodonSequence
+##
+##########################################################################/** 
+#
+# @RdocClass CodonSequence
+# 
+# @title "The CodonSequence class"
+# 
+# \description{ 
+#	Sequence objects aggregating Site objects having a CodonAlphabet attached by default.
+#
+#	@classhierarchy
+# }
+#	
+# @synopsis
+#	
+# \arguments{
+#       \item{name}{Name of the Sequence object.}
+#       \item{string}{A string specifying the length and the states of the Sequence object.}
+#       \item{length}{The length of the sequence. Mutually exclusive with "string".}
+#	\item{table.id}{The genetic code table to use in the attached CodonAlphabet object ("Standard" by default).}
+#       \item{processes}{A list of list of Process objects, to be attached to the aggregated Site objects. Recycled if shorter than the length of the sequence.}
+#       \item{ancestral.obj}{The ancestral object of the Sequence object (a valid Sequence or Process object).}
+#       \item{...}{Not used.}
+#	}
+# 
+# \section{Fields and Methods}{ 
+# 	@allmethods
+# }
+# 
+# \examples{ 
+#       # create an empty CodonSequence object
+#       s<-CodonSequence(length=50)
+#       s
+#       # set states
+#       s$states<-c("ATG","CGA","TTT","CTA")
+#       s
+#       # create a sequence object by specifying a string
+#       s<-CodonSequence(string="ATCTTTCGAATG")
+#       s
+# }
+# 
+# @author
+#
+# \seealso{ 
+# 	@seeclass 
+# }
+# 
+#*/###########################################################################
+setConstructorS3(
+  "CodonSequence",
+  function(
+    name=NA,
+    string=NA,
+    length=NA,
+    table.id=1,
+    processes=NA,
+    ancestral.obj=NA,
+    ...
+	){
+
+		if (!missing(string)){
+
+			len<-((stringLength(string)/3));
+			flen<-floor(len);
+
+			this<-Sequence(length=flen,alphabets=list(CodonAlphabet(table.id=table.id)));
+			if(len > flen){
+				warning("The length of the provided string was not multiple of 3. The incomplete codon was discarded!\n");
+			}
+
+			nuc<-strsplit(string,"")[[1]];
+
+
+			j<-1;	# counter for the codon position
+			for(i in seq(from=1,to=(flen * 3),by=3)){
+
+					# get the codon:
+					state<-paste(nuc[c(i,i+1,i+2)],collapse="");
+
+					# Check for stop codons:
+					if( isStopCodon(this$.sites[[j]]$.alphabet, state) ){
+						throw("The CodonSequence objects does not accept stop codons as valid states!\n");
+					}
+
+					# Set the state:			
+					setStates(this,state,j);		
+					j<-j+1;
+			}
+			
+		}
+		else if(!missing(length)){
+			this<-Sequence(length=length,alphabets=list(CodonAlphabet(table.id=table.id)));
+		}
+		else {
+			this<-Sequence(alphabets=list(CodonAlphabet(table.id=table.id)));
+		}
+
+		this<-extend(this, "CodonSequence");
+		
+		if(!missing(name)){
+			this$name<-name;
+		}
+		
+		if(!missing(processes)){
+			setProcesses(this,processes);
+		}
+	
+		if(!missing(ancestral.obj)){
+			this$.ancestral<-ancestral.obj;
+		}
+
+		return(this);
+
+  },
+  enforceRCC=TRUE
+);
+
+##	
+## Method: checkConsistency
+##	
+###########################################################################/**
+#
+# @RdocMethod	checkConsistency
+# 
+# @title "Check object consistency"
+# 
+# \description{ 
+#		@get "title".
+# } 
+# 
+# @synopsis 
+# 
+# \value{ 
+#		Returns an invisible TRUE if no inconsistencies found in the object, throws 
+#		an error otherwise. 
+# } 
+# 
+# @author 
+# 
+# \seealso{ 
+# 	@seeclass 
+# } 
+# 
+#*/###########################################################################
+setMethodS3(
+	"checkConsistency", 
+	class="CodonSequence", 
+	function(
+		this,
+		...
+	){
+
+      wp<-this$writeProtected;
+      if (wp) {
+        this$writeProtected<-FALSE;
+      }
+			
+		  may.fail<-function(this) {
+	
+				for(pos in seq(along=this$.sites)){
+					if(!is.CodonAlphabet(this$.sites[[pos]]$.alphabet)){
+						throw("The alphabet attached to site ",pos," is not a codon alphabet!\n");
+					}
+				}
+		
+      }
+      tryCatch(may.fail(this),finally=this$writeProtected<-wp);
+			NextMethod();		
+
+	},
+	private=FALSE,
+	protected=FALSE,
+	overwrite=FALSE,
+	conflict="warning",
+	validators=getOption("R.methodsS3:validators:setMethodS3")
+);
+
+##  
+## Method: Translate
+##  
+###########################################################################/**
+#
+# @RdocMethod Translate
+# 
+# @title "Translate a CodonSequence object" 
+# 
+# \description{ 
+#	@get "title".
+# } 
+# 
+# @synopsis 
+# 
+# \arguments{ 
+# 	\item{this}{A CodonSequence object} 
+# 	\item{...}{Not used.} 
+# } 
+# 
+# \value{ 
+# 	The translation as an AminoAcidSequence object.
+# } 
+# 
+# \examples{
+#	# create a CodonSequence object
+#	s<-CodonSequence(string="ATCTTTCGAATGGGGCCCTCCCGA")
+#	# get the translation as an AminoAcidSequence object
+#	as<-Translate(s)
+#	as
+# } 
+# 
+# @author 
+# 
+# \seealso{ 
+# 	@seeclass 
+# } 
+# 
+#*/###########################################################################
+setMethodS3(
+  "Translate",
+  class="CodonSequence",
+  function(
+    this,
+    ...
+  ){ 
+
+			# Create an amino acid sequence of proper length:
+			that<-AminoAcidSequence(
+					name=paste("Translation of",this$name),
+					length=this$length
+			);
+
+			# Setting the states, using the site alphabet to translate codons for greater flexibility.
+			for (pos in seq(along=this$.sites)){
+				setState(that$.sites[[pos]], translateCodon(this$.sites[[pos]]$alphabet, this$.sites[[pos]]$state) );
+			}
+
+			return(that);
+
+  },
+  private=FALSE,
+  protected=FALSE,
+  overwrite=FALSE,
+  conflict="warning",
+  validators=getOption("R.methodsS3:validators:setMethodS3")
+);
 
