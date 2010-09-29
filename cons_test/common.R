52e4d87928c804c7a7a04e98dab4bf2a10ab877d
Botond Sipos <sbotond@ebi.ac.uk>
2010-09-29 17:57:10 +0100

    Updated cons_test/common.R

--- cons_test/common.R
+++ cons_test/common.R
@@ -12,12 +12,24 @@ tree<-read.tree("tree/test_tree.nwk");
 # function iterating over sequence length 
 iterate_seq_len<-function(phylo,lengths,reps,sim.fun,est.fun){
 	res<-list();	
+	times<-list(len=numeric(),rep=numeric(),sim=numeric(),est=numeric(),el.sim=numeric(),el.est=numeric());
+
 	for(len in lengths){
-		for(rep in reps){
-			sim.fun(phylo,len,rep);
-			res<-c(res, est.fun(phylo,len,rep));
-		}
+			seq<-construct_root_sequence(len);
+		for(rep in reps)
+			sim.t<-system.time(sim.fun(phylo,seq,len,rep));
+			est.t<-system.time(est<-est.fun(phylo,len,rep));
+			res<-c(res, est);
+
+			# paste timing info:
+			times$len<-c(times$len,len);
+			times$rep<-c(times$rep,rep);
+			times$sim<-c(times$sim,(as.numeric(sim.t[1])+as.numeric(sim.t[2])));
+			times$est<-c(times$est,(as.numeric(est.t[1])+as.numeric(est.t[2])));
+			times$el.sim<-c(times$el.sim,as.numeric(sim.t[3]));
+			times$el.est<-c(times$el.est,as.numeric(est.t[3]));
 	}
-	return(res);
+
+	return(list("res"=res,"times"=as.data.frame(times)));
 }
 
